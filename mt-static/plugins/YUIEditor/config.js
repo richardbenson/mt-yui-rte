5679edb5f1bc7bea370d5be205609a49e396a9b2
Richard Benson <me@richardbenson.co.uk>
2009-03-15 21:54:43 +0000

    Import old versions

---
+++ mt-static/plugins/YUIEditor/config.js
@@ -0,0 +1,8 @@
+//Config file for MT YUI integration
+
+function afterYUIInit() {
+	//Function for your own customisations to YUI, called after load
+	var YUIEditor = app.editor.myEditor;
+	/*All the functions, objects, contructors etc of YUI are available by referencing YUIEditor e.g.:
+	//YUIEditor.toolbar.addButtonToGroup({ type: 'push', label: 'Test Button', value: 'testbutton' }, 'insertitem')*/
+};
---
+++ mt-static/plugins/YUIEditor/js/load.js
@@ -0,0 +1,16 @@
+function LoadYUI() {
+	
+	//Load the YUI CSS
+	document.write(unescape("%3Clink rel='stylesheet' href='" + ConfigYUIBase + "assets/skins/sam/skin.css' type='text/css'%3E"));
+	
+	//Utility dependencies
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "yahoo-dom-event/yahoo-dom-event.js' type='text/javascript'%3E%3C/script%3E"));
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "element/element-beta-min.js' type='text/javascript'%3E%3C/script%3E"));
+	//Needed for Menus, Buttons and Overlays used in the Toolbar
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "container/container_core-min.js' type='text/javascript'%3E%3C/script%3E"));
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "menu/menu-min.js' type='text/javascript'%3E%3C/script%3E"));
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "button/button-min.js' type='text/javascript'%3E%3C/script%3E"));
+	//Source file for Rich Text Editor
+	document.write(unescape("%3Cscript src='" + ConfigYUIBase + "editor/editor-min.js' type='text/javascript'%3E%3C/script%3E"));
+
+};
