828fc4151935f07a07f1897429b6d4a55045eee3
Richard Benson <me@richardbenson.co.uk>
2009-03-15 21:48:59 +0000

    Import old versions

---
+++ mt-static/plugins/YUIEditor/config.js
@@ -0,0 +1,10 @@
+//Config file for MT YUI integration
+
+//Set this to false to use the default YUI image loader
+var ConfigUseMTAssets = true;
+
+//true = remove form tags but leave comments, false = leave form tags alone
+var ConfigStripFormTags = true;
+
+//true makes the editor window automatically resize to fit content
+var ConfigAutoHeight = true;
