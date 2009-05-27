6912ea009a23ccfe81cfda2dbd1ba822c5255033
Richard Benson <me@richardbenson.co.uk>
2009-05-27 16:02:08 +0000

    Remove temp JS settings

--- mt-static/plugins/YUIEditor/config.js
+++ mt-static/plugins/YUIEditor/config.js
@@ -1,10 +1,3 @@
-        var ConfigYUIBase = "http://yui.yahooapis.com/2.7.0/build/";
-        var ConfigUseMTAssets = true;
-        var ConfigStripFormTags = true;
-        var ConfigAutoHeight = true;
-        var ConfigMarkupType = "xhtml";
-        var ConfigYahooAppID = "nmX1J8TV34ErpYjds0Kk18_i9hCQM9OqtBoeuS6PYdSm0hm6d3L0xbRz6WZdsw--";
-
 //Config file for MT YUI integration
 
 function afterYUIInit() {
